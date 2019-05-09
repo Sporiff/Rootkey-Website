b1d0a44c496f8f4b70d3d55b03f202db83283509
Ciaran Ainsworth <ciaran.ainsworth@oneserve.co.uk>
2019-05-09 12:42:49 +0100

    New post, update gitignore files

--- Website/conf.py
+++ Website/conf.py
@@ -261,9 +261,7 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'github_button': True,
-    'github_user': 'Sporiff',
-    'github_repo': 'rootkey-blog'
+    'github_button': False,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
